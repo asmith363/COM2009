352433eac5987ff38b6fa1bd21399f670c9c175a
Luke Peacock <lukepeacock13@gmail.com>
2019-03-08 10:00:47 +0000

    added comments

--- COM2009-Assignment-A8/COM2009-Assignment-A8.py
+++ COM2009-Assignment-A8/COM2009-Assignment-A8.py
@@ -42,11 +42,15 @@ def set_font(name):
     '''
     os.system('setfont ' + name)
 
+#
 def measure_distance(us3):
- #Code for Q2, take sample of distance every 10 msec and work out mean,min,max,standard deviation  
+    """ 
+        This function takes 10 distance samples and averages them.
+        It the returns the average distance.
+    """
     #Time loop begins
     total = 0
-    #Take 1000 samples 
+    #Take 10 samples 
     for j in range (0,10):
         starttime = time.time()
         dis = us3.value()
@@ -61,7 +65,8 @@ def measure_distance(us3):
     
 
 def main():
-    '''The main function of our program'''
+    '''The main function of the program
+        It sets all the paremeters and contains the PID controller'''
     # set the console just how we want it
     reset_console()
     set_cursor(OFF)
@@ -81,50 +86,56 @@ def main():
     us2 = ev3.UltrasonicSensor('in2')
     
     
-    #Set values
+    #Set values for PID
     Kp = 3 * 100
     Ki = 9.6* 100
     Kd = 0.234375 * 100
+    #Initialise integral and last error/goal
     integral = 0
     lastError = 0
     lastGoal = 0
     derivative = 0
+    #set dT time and base speed
     dT= 0.2
     sp = 50
+    #Run PID controller indefinitely
     while True:
         starttime = time.time()
         #pid controller 
-        rightDis = measure_distance(us2)
-        leftDis = measure_distance(us3)
-        goal = (leftDis + rightDis) / 2
+        right_dis = measure_distance(us2)
+        left_dis = measure_distance(us3)
+        goal = (left_dis + right_dis) / 2
        
+        #If the goals is different to last goal then reset integral and derivative
         if goal != lastGoal:   
             integral = 0
             derivative = 0  
         
-        #take 100 samples over 1 second
-        dis = leftDis
+        #take 10 samples over 0.1 second
+        dis = left_dis
         error = dis - goal
         #set integral to 0 when error changes sign
         if (math.copysign(1, lastError) != math.copysign(1, error)):
             integral = 0
+        #dampen and calculate new integral and derivative
         integral = ((2*integral)/3) + error
         derivative = error - lastError
+
+        #Calculate move control (PID)
         move = (Kp * error) + (Ki * integral * dT) + (Kd * derivative/dT)
         move = move  / 100
+        #Cap move to be -25 < move < 25
         if move > 25:
             move = 25
         if move < -25:
             move = -25
-        if dis == goal:
-            mb.run_direct(duty_cycle_sp=0)
-            mc.run_direct(duty_cycle_sp=0)
-        else: 
-            mb.run_direct(duty_cycle_sp= sp+move)
-            mc.run_direct(duty_cycle_sp= sp-move)
+        #Run motors
+        mb.run_direct(duty_cycle_sp= sp+move)
+        mc.run_direct(duty_cycle_sp= sp-move)
+        #set last error and goal
         lastError = error
         lastGoal = goal
-        debug_print("left: " , leftDis, " right: ", rightDis, " goal", goal, " dis", dis)
+        #sleep for time dT
         time.sleep(dT - (time.time() - starttime))
           
 
